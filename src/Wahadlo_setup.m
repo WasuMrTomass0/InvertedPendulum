511559014b717cab9269ba882c290b97f3bcf9c6
WasuPC <pan.t@op.pl>
2021-03-16 13:47:57 +0100

    Lab 2 - model matematyczny

---
+++ src/Wahadlo_setup.m
@@ -0,0 +1,26 @@
+% % % % Stale:
+g = 9.8101;
+m_c = 0.503 + 0.045;
+m_p = 2 * (20.5 + 11);  
+% 2 * 0.055; wg napisu
+% f_c = 
+% f_p = 
+
+% dlugosc wahadla xD aby policzyć j_p
+% Wahadlo:
+% odwaznik  11      mosiezny
+% pret      20,5    aluminium
+
+% % % % Wartosci wyliczane:
+m = m_c + m_p;
+% j_p = 
+% L = 
+mL = m * L;
+
+a_1 = j_p / (m * L);
+a_2 = 1 / L;
+b = a_1 * a_2;
+k_3 = f_p / ( m * L);
+
+% TODO w simulinku
+% wyliczanie p1 i p2
