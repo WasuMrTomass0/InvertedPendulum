ca93df34170e51d3fff20365b2082562785e6a5d
Jakub Poręba <spikasny@gmail.com>
2021-04-18 19:35:03 +0200

    Add finally working perfect clean mathematical model pendulum; add example solution from jtjt; update gitignore

--- src/clean_pendulum_setup.m
+++ src/clean_pendulum_setup.m
@@ -4,14 +4,28 @@ M = 0.503 + 0.045;  % masa wozka
 f_c = 1.0942;  %0.5;          % tarcie wozka
 f_p = 0.10436; %6.65e-5;      % tarcie rotacyjne wahadla
 
+% Masa elementow wahadel
 m_pw = 2 * 11 / 1000;
 m_ps = 2 * 20.5 / 1000;
-r_c = 5.5 / 1000;
-l_c = 19 / 1000;
-r_p = 3 / 1000;
+
+% Wymiary elementow wahadel
+r_c = 8.3 / 1000;
+l_c = 1.9 / 100;
+r_p = 4.7 / 1000;
 l_p = 43 / 100;
 l_c0 = 39.6 / 100;
 l_p0 = 18.1 / 100;
+
+
+L = 0.2899;          % odleglosc srodka masy od osi obrotu
+m = m_pw + m_ps;    % calkowita masa wahadla
+g = 9.81;
+
+% Momenty bezwladnosci
+% I1 = m*(3*r_p^2 + l_p)/12 + m_ps*(l_p/2)^2;
+% I2 = m*(3*r_c^2 + l_c)/12 + m_pw*((l_c/2)^2 + l_p);
+% I = I1 + I2;
+
 I = 1/12 * m_pw * l_c ^ 2 ...
     + 1/4 * m_pw * r_c ^ 2 ...
     + m_pw * l_c0 ^ 2 ...
@@ -19,10 +33,7 @@ I = 1/12 * m_pw * l_c ^ 2 ...
     + 1/4 * m_pw * r_p ^ 2 ...
     + m_ps * l_p0 ^ 2;
 
-L = 0.256;          % odleglosc srodka masy od osi obrotu
-m = m_pw + m_ps;    % calkowita masa wahadla
-g = 9.81;
-
+T_teor = 2*pi*sqrt(I/(m*g*L));
 
 % Pomiary poczatkowe:
 % Okres 
